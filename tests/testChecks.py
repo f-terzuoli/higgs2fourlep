c431ad7bdc14b705df03b4cf3b394e070a1cf9f5
f.terzuoli <55702672+f-terzuoli@users.noreply.github.com>
2022-04-22 00:42:56 +0200

    Update and rename testchecks.py to testChecks.py

---
+++ tests/testChecks.py
@@ -0,0 +1,7 @@
+import logging, sys
+from higgs2fourlep import checkRequirements
+
+logging.basicConfig(filename='reqlog.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%d/%m/%Y %H:%M:%S', filemode = "w+")
+screen_handler = logging.StreamHandler(stream=sys.stdout)
+logging.getLogger().addHandler(screen_handler)
+checkRequirements()
