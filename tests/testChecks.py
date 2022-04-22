7d23aba7e1e989d9b6432e4dc53c4d18175da113
f.terzuoli <55702672+f-terzuoli@users.noreply.github.com>
2022-04-22 02:09:38 +0200

    Update testChecks.py

--- tests/testChecks.py
+++ tests/testChecks.py
@@ -1,5 +1,5 @@
 import logging, sys
-from higgs2fourlep import checkRequirements
+from ..higgs2fourlep import checkRequirements
 
 logging.basicConfig(filename='reqlog.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%d/%m/%Y %H:%M:%S', filemode = "w+")
 screen_handler = logging.StreamHandler(stream=sys.stdout)
