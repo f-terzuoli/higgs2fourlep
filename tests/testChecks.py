2a1613a163bd565318c27c9801a18fec389b6ddb
f.terzuoli <55702672+f-terzuoli@users.noreply.github.com>
2022-04-22 02:33:17 +0200

    Update testChecks.py

--- tests/testChecks.py
+++ tests/testChecks.py
@@ -1,3 +1,10 @@
+import os
+current_dir = os.path.dirname(os.path.realpath(__file__)))
+working_dir = os.path.join(current_dir , "..")
+
+import sys
+sys.path.append(working_dir)
+
 import logging, sys
 from higgs2fourlep import checkRequirements
 
